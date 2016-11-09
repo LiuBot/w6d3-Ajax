6fb5a228d35e3762b3866a06f20da62c2e4cbaf1
Sophia Liu <sliu3@wellesley.edu>
2016-11-09 13:32:04 -0800

    ajax twitter

---
+++ ajaxTwitter/frontend/follow_toggle.js
@@ -0,0 +1,8 @@
+// Now let's start writing the code for the 
+// FollowToggle class. Create a new file in 
+// frontend/ called follow_toggle.js.
+
+// NB: Each of your JavaScript classes for this 
+// project should be in a separate file in 
+// frontend/. Name the files in snake_case.
+
